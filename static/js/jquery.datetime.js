66bf4204b474dff5e5fd2968986bf0bee9478354
Marc Hoersken <info@marc-hoersken.de>
2013-04-02 19:49:14 +0200

    Optimized JavaScript code

--- static/js/jquery.datetime.js
+++ static/js/jquery.datetime.js
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (c) 2009 Marc Hörsken <info@marc-hoersken.de>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,10 +28,10 @@ Date.prototype.setISO8601 = function(string) {
       "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
       "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
   var d = string.match(new RegExp(regexp));
-  
+
   var offset = 0;
   var date = new Date(d[1], 0, 1);
-  
+
   if (d[3]) { date.setMonth(d[3] - 1); }
   if (d[5]) { date.setDate(d[5]); }
   if (d[7]) { date.setHours(d[7]); }
@@ -42,63 +42,46 @@ Date.prototype.setISO8601 = function(string) {
     offset = (Number(d[16]) * 60) + Number(d[17]);
     offset *= ((d[15] == '-') ? 1 : -1);
   }
-  
+
   offset -= date.getTimezoneOffset();
   time = (Number(date) + (offset * 60 * 1000));
   this.setTime(Number(time));
-  
+
   return this;
 };
-jQuery.fn.extend({
-  formatDatetime: function() {
-    jQuery(this).find('time.date[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toDateString());
-      elem.removeClass('date');
+Date.prototype.toDateTimeString = function() {
+  return this.toDateString() + ' ' + this.toTimeString();
+};
+Date.prototype.toLocaleDateTimeString = function() {
+  return this.toLocaleDateString() + ' ' + this.toLocaleTimeString();
+};
+(function($) {
+    $.fn.extend({
+      formatDatetime: function() {
+        this.find('time.date[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toDateString()).removeClass('date');
+        });
+        this.find('time.time[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toTimeString()).removeClass('time');
+        });
+        this.find('time.datetime[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toDateTimeString()).removeClass('datetime');
+        });
+        this.find('time.localedate[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toLocaleDateString()).removeClass('localedate');
+        });
+        this.find('time.localetime[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toLocaleTimeString()).removeClass('localetime');
+        });
+        this.find('time.localedatetime[datetime]').each(function(i) {
+          $(this).text(new Date().setISO8601(this.getAttribute('datetime')).toLocaleDateTimeString()).removeClass('localedatetime');
+        });
+        return this;
+      }
     });
-    jQuery(this).find('time.time[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toTimeString());
-      elem.removeClass('time');
+    $(document).ready(function() {
+      $(document).formatDatetime();
+    }).ajaxComplete(function(request, settings) {
+      $(document).formatDatetime();
     });
-    jQuery(this).find('time.datetime[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toDateString() + ' ' + date.toTimeString());
-      elem.removeClass('datetime');
-    });
-    jQuery(this).find('time.localedate[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toLocaleDateString());
-      elem.removeClass('localedate');
-    });
-    jQuery(this).find('time.localetime[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toLocaleTimeString());
-      elem.removeClass('localetime');
-    });
-    jQuery(this).find('time.localedatetime[datetime]').each(function(i) {
-      var elem = jQuery(this);
-      var date = new Date();
-      date.setISO8601(elem.attr('datetime'));
-      elem.text(date.toLocaleDateString() + ' ' + date.toLocaleTimeString());
-      elem.removeClass('localedatetime');
-    });
-    return jQuery(this);
-  }
-});
-jQuery(document).ready(function() {
-  jQuery(document).formatDatetime();
-});
-jQuery(document).ajaxComplete(function(request, settings) {
-  jQuery(document).formatDatetime();
-});
+})(jQuery);
